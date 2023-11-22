9a2efbc2683691ce8945bf07cb9955d485ccc02c
Linden <70739041+linden@users.noreply.github.com>
2023-11-21 20:29:53 -0800

    chore: remove incomplete test (`TestForEach`)

--- tempdb_test.go
+++ tempdb_test.go
@@ -3,7 +3,6 @@ package tempdb
 import (
 	"log/slog"
 	"os"
-	"strconv"
 	"testing"
 
 	"github.com/btcsuite/btcwallet/walletdb/walletdbtest"
@@ -32,31 +31,4 @@ func TestInterface(t *testing.T) {
 	Logger = slog.Default()
 
 	walletdbtest.TestInterface(t, "tempdb", "test.db")
-}
-
-func TestForEach(t *testing.T) {
-	db, err := New("/test")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	tx, err := db.BeginReadWriteTx()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bkt, err := tx.CreateTopLevelBucket([]byte("top bucket"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	nBkt, err := bkt.CreateBucket([]byte("nested bucket"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for i := 0; i < 5; i++ {
-		key := strconv.Itoa(i) + "bkt"
-		nBkt.Put([]byte(key), []byte(key+"'s value"))
-	}
 }

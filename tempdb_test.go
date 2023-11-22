7a4bab424082863397e2ef51f42619418297a092
Linden <70739041+linden@users.noreply.github.com>
2023-11-21 20:29:14 -0800

    chore: hide timestamp & level in tests

--- tempdb_test.go
+++ tempdb_test.go
@@ -13,6 +13,14 @@ func TestMain(m *testing.M) {
 	// create a new logger.
 	h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
+		ReplaceAttr: func(groups []string, attr slog.Attr) slog.Attr {
+			// hide the timestamp and level key.
+			if attr.Key == slog.TimeKey || attr.Key == slog.LevelKey {
+				return slog.Attr{}
+			}
+
+			return attr
+		},
 	})
 	Logger = slog.New(h)
 

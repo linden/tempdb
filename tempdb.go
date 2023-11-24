42fe18a60308926f854b913f72db2e8dbfa0b4cc
Linden <70739041+linden@users.noreply.github.com>
2023-11-24 15:00:14 -0800

    merge(#4): open should return an error
    Co-authored-by: Ellis Frank <ellis@mcfranko.com>

--- tempdb.go
+++ tempdb.go
@@ -107,12 +107,17 @@ func New(args ...any) (walletdb.DB, error) {
 	}, nil
 }
 
+// Open always returns `walletdb.ErrDbDoesNotExist` because tempdb is not persistent.
+func Open(args ...any) (walletdb.DB, error) {
+	return nil, walletdb.ErrDbDoesNotExist
+}
+
 func init() {
 	err := walletdb.RegisterDriver(walletdb.Driver{
 		DbType: "tempdb",
 
 		Create: New,
-		Open:   New,
+		Open:   Open,
 	})
 
 	if err != nil {
